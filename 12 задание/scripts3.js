3e1541d02a7de18fec270a25841e40c2082960d0
Сергей Пергун <96252268+1channel@users.noreply.github.com>
2022-06-01 13:39:29 +0300

    Update scripts3.js

--- 12 задание/scripts3.js
+++ 12 задание/scripts3.js
@@ -9,12 +9,12 @@ if (time >= 8 && time < 13 || time >= 14 && time < 19)
   go_to_dairy = true;
   alert('Фабрика: ' + go_to_dairy);
 }
-else if (time >= 9 && time < 14 || time >= 15 && time < 17)
+elif (time >= 9 && time < 14 || time >= 15 && time < 17)
 {
   go_to_store = true;
   alert('Магазин: ' + go_to_store);
 }
-else if (time >= 7 && time < 20)
+elif (time >= 7 && time < 20)
 {
   go_to_market = true;
   alert('Рынок: ' + go_to_market);
